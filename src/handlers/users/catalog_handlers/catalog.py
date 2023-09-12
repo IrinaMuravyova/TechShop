8e7026fc202288c6ee500f93010eff7333295337
IrinaMuravyova <miomir@yandex.ru>
2023-09-12 22:23:37 +0800

    Переписала catalig.py - убрала дублирующий код

--- src/handlers/users/catalog_handlers/catalog.py
+++ src/handlers/users/catalog_handlers/catalog.py
@@ -11,8 +11,21 @@ from aiogram.dispatcher import FSMContext
 from states import ShowStates
 
 
-@dp.callback_query_handler(navigation_items_callback.filter(for_data='notebooks'))
-async def notebooks(call: types.CallbackQuery, state: FSMContext):
+@dp.callback_query_handler(navigation_items_callback.filter())
+async def notebooks(call: types.CallbackQuery, state: FSMContext, callback_data: dict):
+    match callback_data.get('for_data'):
+       case 'notebooks':   category_id=1 # category = 'Ноутбуки'
+       case 'smartphones': category_id=2 # category = 'Смартфоны'
+       case 'notepads':    category_id=3 # category = 'Планшеты'
+       case 'tvs':         category_id=4 # category = 'Телевизоры'
+       case 'computers':   category_id=5 # category = 'Компьютеры'
+       case 'screens':     category_id=6 # category = 'Мониторы'
+       case 'pstations':   category_id=7 # category = 'Игровые приставки'
+       case 'vcards':      category_id=8 # category = 'Видеокарты'
+       case 'components':  category_id=9 # category = 'Комплектующие'
+       case 'accessories': category_id=10 # category = 'Аксессуары'
+       case 'forhome':     category_id=11 # category = 'Для дома'
+        
 
     chat_id = call.message.chat.id
     message_id = call.message.message_id
@@ -21,167 +34,5 @@ async def notebooks(call: types.CallbackQuery, state: FSMContext):
     await bot.edit_message_text(text=text+f'\n{hbold("Ноутбуки")}', 
                                     chat_id=chat_id, 
                                     message_id=message_id,
-                                    #reply_markup=notebooks_keyboard
-                                    reply_markup=get_brands_models_inline_keyboard(category_id=1)
+                                    reply_markup=get_brands_models_inline_keyboard(category_id=category_id)
                                     )
-
-        
-@dp.callback_query_handler(navigation_items_callback.filter(for_data='smartphones'))
-async def smartphones(call: types.CallbackQuery, state: FSMContext):
-
-    chat_id = call.message.chat.id
-    message_id = call.message.message_id
-    text = f'Выберите производителя устройства\n-----'
-
-    
-    await bot.edit_message_text(text=text+f'\n{hbold("Смартфоны")}', 
-                                    chat_id=chat_id, 
-                                    message_id=message_id,
-                                    reply_markup=get_brands_models_inline_keyboard(category_id=2)#smartphones_keyboard
-                                    )
-
-@dp.callback_query_handler(navigation_items_callback.filter(for_data='notepads'))
-async def notepads(call: types.CallbackQuery, state: FSMContext):
-
-    for_data = call.data.split(':')[-1]
-
-    chat_id = call.message.chat.id
-    message_id = call.message.message_id
-    text = f'Выберите производителя устройства\n-----'
-    text_back = f'Выберите категорию устройств\n-----'
-    
-    await bot.edit_message_text(text=text+f'\n{hbold("Планшеты")}', 
-                                    chat_id=chat_id, 
-                                    message_id=message_id,
-                                    reply_markup=get_brands_models_inline_keyboard(category_id=3)#notepads_keyboard
-                                    )    
-
-@dp.callback_query_handler(navigation_items_callback.filter(for_data='tvs'))
-async def tvs(call: types.CallbackQuery, state: FSMContext):
-
-    for_data = call.data.split(':')[-1]
-
-    chat_id = call.message.chat.id
-    message_id = call.message.message_id
-    text = f'Выберите производителя устройства\n-----'
-    text_back = f'Выберите категорию устройств\n-----'
-    
-    await bot.edit_message_text(text=text+f'\n{hbold("Телевизоры")}', 
-                                    chat_id=chat_id, 
-                                    message_id=message_id,
-                                    reply_markup=get_brands_models_inline_keyboard(category_id=4)#tvs_keyboard
-                                    ) 
-
-@dp.callback_query_handler(navigation_items_callback.filter(for_data='computers'))
-async def computers(call: types.CallbackQuery, state: FSMContext):
-
-    for_data = call.data.split(':')[-1]
-
-    chat_id = call.message.chat.id
-    message_id = call.message.message_id
-    text = f'Выберите производителя устройства\n-----'
-    text_back = f'Выберите категорию устройств\n-----'
-    
-    await bot.edit_message_text(text=text+f'\n{hbold("Компьютеры")}', 
-                                    chat_id=chat_id, 
-                                    message_id=message_id,
-                                    reply_markup=get_brands_models_inline_keyboard(category_id=5)#computers_keyboard
-                                    ) 
-
-@dp.callback_query_handler(navigation_items_callback.filter(for_data='screens'))
-async def screens(call: types.CallbackQuery, state: FSMContext):
-
-    for_data = call.data.split(':')[-1]
-
-    chat_id = call.message.chat.id
-    message_id = call.message.message_id
-    text = f'Выберите производителя устройства\n-----'
-    text_back = f'Выберите категорию устройств\n-----'
-    
-    await bot.edit_message_text(text=text+f'\n{hbold("Мониторы")}', 
-                                    chat_id=chat_id, 
-                                    message_id=message_id,
-                                    reply_markup=get_brands_models_inline_keyboard(category_id=6)#screens_keyboard
-                                    )  
-
-@dp.callback_query_handler(navigation_items_callback.filter(for_data='pstations'))
-async def pstations(call: types.CallbackQuery, state: FSMContext):
-
-    for_data = call.data.split(':')[-1]
-
-    chat_id = call.message.chat.id
-    message_id = call.message.message_id
-    text = f'Выберите производителя устройства\n-----'
-    text_back = f'Выберите категорию устройств\n-----'
-    
-    await bot.edit_message_text(text=text+f'\n{hbold("Игровые приставки")}', 
-                                    chat_id=chat_id, 
-                                    message_id=message_id,
-                                    reply_markup=get_brands_models_inline_keyboard(category_id=7)#pstations_keyboard
-                                    ) 
-
-@dp.callback_query_handler(navigation_items_callback.filter(for_data='vcards'))
-async def vcards(call: types.CallbackQuery, state: FSMContext):
-
-    for_data = call.data.split(':')[-1]
-
-    chat_id = call.message.chat.id
-    message_id = call.message.message_id
-    text = f'Выберите производителя устройства\n-----'
-    text_back = f'Выберите категорию устройств\n-----'
-    
-    await bot.edit_message_text(text=text+f'\n{hbold("Видеокарты")}', 
-                                    chat_id=chat_id, 
-                                    message_id=message_id,
-                                    reply_markup=get_brands_models_inline_keyboard(category_id=8)#vcards_keyboard
-                                    ) 
-
-
-@dp.callback_query_handler(navigation_items_callback.filter(for_data='components'))
-async def components(call: types.CallbackQuery, state: FSMContext):
-
-    for_data = call.data.split(':')[-1]
-
-    chat_id = call.message.chat.id
-    message_id = call.message.message_id
-    text = f'Выберите производителя устройства\n-----'
-    text_back = f'Выберите категорию устройств\n-----'
-    
-    await bot.edit_message_text(text=text+f'\n{hbold("Комплектующие")}', 
-                                    chat_id=chat_id, 
-                                    message_id=message_id,
-                                    reply_markup=get_brands_models_inline_keyboard(category_id=9)#components_keyboard
-                                    ) 
-        
-
-@dp.callback_query_handler(navigation_items_callback.filter(for_data='accessories'))
-async def accessories(call: types.CallbackQuery, state: FSMContext):
-
-    for_data = call.data.split(':')[-1]
-
-    chat_id = call.message.chat.id
-    message_id = call.message.message_id
-    text = f'Выберите производителя устройства\n-----'
-    text_back = f'Выберите категорию устройств\n-----'
-    
-    await bot.edit_message_text(text=text+f'\n{hbold("Аксессуары")}', 
-                                    chat_id=chat_id, 
-                                    message_id=message_id,
-                                    reply_markup=get_brands_models_inline_keyboard(category_id=10)#accessories_keyboard
-                                    )
-
-@dp.callback_query_handler(navigation_items_callback.filter(for_data='forhome'))
-async def forhome(call: types.CallbackQuery, state: FSMContext):
-
-    for_data = call.data.split(':')[-1]
-
-    chat_id = call.message.chat.id
-    message_id = call.message.message_id
-    text = f'Выберите производителя устройства\n-----'
-    text_back = f'Выберите категорию устройств\n-----'
-    
-    await bot.edit_message_text(text=text+f'\n{hbold("Для дома")}', 
-                                    chat_id=chat_id, 
-                                    message_id=message_id,
-                                    reply_markup=get_brands_models_inline_keyboard(category_id=11)#forhome_keyboard
-                                    )    
